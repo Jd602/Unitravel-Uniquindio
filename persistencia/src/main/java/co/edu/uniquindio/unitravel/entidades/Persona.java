2d4f7f701c684607dd8dab3c9e600662eb639cb1
jd602 <juand.sarazan@uqvirtual.edu.co>
2022-04-27 07:16:27 -0500

    Entidades persona y telefono, gitignore

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/Persona.java
@@ -0,0 +1,95 @@
+package co.edu.uniquindio.unitravel.entidades;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@Entity
+public class Persona implements Serializable {
+
+    @Id
+    private String cedula;
+
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String direccion;
+    @ElementCollection
+    private Map<String,String> telefono;
+
+    public Persona() {
+    }
+
+    public Persona(String cedula, String nombre, String apellido, String correo, String direccion) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Map<String, String> getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Map<String, String> telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(cedula, persona.cedula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cedula);
+    }
+}
